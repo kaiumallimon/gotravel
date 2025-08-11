392e6e8581ab1cad0c5a072081213d22253050dd
Kaium Al Limon <limon.3cs@gmail.com>
2025-08-11 18:21:41 +0600

    feat: Add app routes for navigation

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const String splash = '/';
+  static const String welcome = '/welcome';
+  static const String login = '/login';
+  static const String register = '/register';
+}
