93d93bfd1685169323d7f58ef1ace2724206f915
Kaium Al Limon <limon.3cs@gmail.com>
2025-10-18 06:36:19 +0600

    fix: Remove invisible character from import statement in search_page.dart

--- lib/presentation/views/user/pages/search_page.dart
+++ lib/presentation/views/user/pages/search_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+﻿import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:gotravel/data/models/place_model.dart';
